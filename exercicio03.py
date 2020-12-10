824e05e3319ab89f45d613d4d2aec251a864f5f1
Guna-ME <Dudamarcos151@hotmail.com>
2020-12-10 11:19:46 -0300

    Fix: adiciona exercicio 03

--- exercicio03.py
+++ exercicio03.py
@@ -1,7 +1,6 @@
-produto = int(input('Digite o valor do produto: R$'))
-
-def pagamento(variavel):
-    opc = int(input('''
+produto = float(input('Digite o valor do produto escolhido: R$'))
+while produto != 0:
+    opc = int(input('''Digite a forma de pagamento:
     1- A vista
     2- Prazo 1x no cartão
     3- Prazo da loja 5x
@@ -12,24 +11,26 @@ def pagamento(variavel):
         print('Valor original:     R$', produto)
         print('Desconto ganho:     R$', produto * 0.50)
         print('Valor com desconto: R$', produto * 0.50)
-        consultar()
+        break
 
     elif opc==2:
         print('Valor original:     R$', produto)
         print('Desconto ganho:     R$', produto * 0.10)
         print('Valor com desconto: R$', produto * 0.90)
+        break
 
     elif opc==3:
         print('Valor original:     R$', produto)
         print('Taxa:               R$', produto * 0.2)
-        print('Valor com taxa:     R$', produto + 10%)
+        print('Valor com taxa:     R$', produto + (produto * 0.10))
         print('Valor das parcelas: R$', produto / 5)
-        consultar()
+        break
 
     elif opc==4:
         print('Valor original:     R$', produto)
         print('Taxa:               R$', produto * 0.2)
-        print('Valor com taxa:     R$', produto + 50%)
+        print('Valor com taxa:     R$', produto + (produto * 0.10))
         print('Valor das parcelas: R$', produto / 10)
-        consultar()
+        break
+    
    
